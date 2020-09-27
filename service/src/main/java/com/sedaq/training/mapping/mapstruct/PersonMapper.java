fa292f767eb6dae2ae03dc557c74174599496ba0
SedaQ <pavelseda@email.cz>
2020-09-27 14:12:59 +0200

    add example MapStruct mapping.

--- service/src/main/java/com/sedaq/training/mapping/mapstruct/PersonMapper.java
+++ service/src/main/java/com/sedaq/training/mapping/mapstruct/PersonMapper.java
@@ -1,4 +1,13 @@
 package com.sedaq.training.mapping.mapstruct;
 
+import com.sedaq.training.api.dto.PersonDTO;
+import com.sedaq.training.persistence.model.Person;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PersonMapper {
+
+    Person mapToEntity(PersonDTO personDTO);
+
+    PersonDTO mapToDto(Person person);
 }
